04d66511633965e3fd433dfff490a3f4d88a6c23
Khathu Makatu <kha2maka2@gmail.com>
2023-09-11 18:08:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,14 +95,14 @@ if selected_feature == "Medical Records Access":
     st.write("Upload your medical records file:")
     uploaded_file = st.file_uploader("Choose a file...", type=["pdf", "jpg", "png"])
 
+# Add a creative message about matching with the WHO database
+        st.write("Your medical records will be securely stored and cross-referenced with the WHO database to provide you with the best healthcare recommendations.")
+    
     # Display the uploaded file
     if uploaded_file is not None:
         st.success("File uploaded successfully!")
         st.write("File Name:", uploaded_file.name)
         st.write("File Size:", uploaded_file.size, "bytes")
-
-        # Add a creative message about matching with the WHO database
-        st.write("Your medical records will be securely stored and cross-referenced with the WHO database to provide you with the best healthcare recommendations.")
 
 if selected_feature == "Symptom Checker":
     st.subheader("Symptom Checker")
