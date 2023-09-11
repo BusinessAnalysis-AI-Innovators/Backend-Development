768696654aa3da0fc72aa0c9d54e9fdfbcfde66c
Khathu Makatu <kha2maka2@gmail.com>
2023-09-11 17:24:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,14 @@ st.write("Empowering You with Easy Access to Quality Healthcare Services")
 # Create a sidebar for buttons with a divider
 with st.sidebar:
     st.subheader("Select a Feature:")
-    if st.button("User Registration & Login"):
-        st.write("User Registration & Login Feature Selected")
+    selected_feature = st.selectbox("", [
+        "User Registration & Login",
+        "Profile Management",
+        "Appointment Scheduling",
+        "Medical Records Access",
+        "Symptom Checker",
+    ])
+    if selected_feature == "User Registration & Login":
         st.subheader("User Registration / Login Area:")
         username = st.text_input("Username", "")
         password = st.text_input("Password", "", type="password")
@@ -44,21 +50,6 @@ with st.sidebar:
 st.markdown('<div class="divider"></div>', unsafe_allow_html=True)
 
 # Display feature details or registration/login area
-feature_selected = st.session_state.get("feature_selected", "")
+if selected_feature:
+    st.write(f"This is where the {selected_feature} details will appear.")
 
-if feature_selected == "User Registration & Login":
-    st.write("This is where the User Registration / Login Area details will appear.")
-else:
-    st.write("This is where the selected feature details will appear.")
-
-# Update the selected feature based on the button clicked
-if st.button("User Registration & Login"):
-    st.session_state.feature_selected = "User Registration & Login"
-elif st.button("Profile Management"):
-    st.session_state.feature_selected = "Profile Management"
-elif st.button("Appointment Scheduling"):
-    st.session_state.feature_selected = "Appointment Scheduling"
-elif st.button("Medical Records Access"):
-    st.session_state.feature_selected = "Medical Records Access"
-elif st.button("Symptom Checker"):
-    st.session_state.feature_selected = "Symptom Checker"
