c1a9f51e71ad9b29277173bf5b80c147ed61a93e
Khathu Makatu <kha2maka2@gmail.com>
2023-09-11 16:54:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,31 +15,28 @@ st.markdown(
 # Add a heading
 st.header("HealthTele-Ease")
 
-# Create buttons in a horizontal row
-button_col1, button_col2, button_col3, button_col4, button_col5 = st.beta_columns(5)
-
 # Button 1: User Registration & Login
-if button_col1.button("User Registration & Login"):
+if st.button("User Registration & Login"):
     # Add your code for this button's functionality here
     pass
 
 # Button 2: Profile Management
-if button_col2.button("Profile Management"):
+if st.button("Profile Management"):
     # Add your code for this button's functionality here
     pass
 
 # Button 3: Appointment Scheduling
-if button_col3.button("Appointment Scheduling"):
+if st.button("Appointment Scheduling"):
     # Add your code for this button's functionality here
     pass
 
 # Button 4: Medical Records Access
-if button_col4.button("Medical Records Access"):
+if st.button("Medical Records Access"):
     # Add your code for this button's functionality here
     pass
 
 # Button 5: Symptom Checker
-if button_col5.button("Symptom Checker"):
+if st.button("Symptom Checker"):
     # Add your code for this button's functionality here
     pass
 
