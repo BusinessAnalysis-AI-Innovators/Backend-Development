b2c8642b1b50d85dc72fdf9758ed6f965bac9b32
Khathu Makatu <kha2maka2@gmail.com>
2023-09-11 17:31:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,27 +21,27 @@ st.title("Welcome to TeleHealth-Ease")
 # Add an introduction for patients
 st.write("Empowering You with Easy Access to Quality Healthcare Services")
 
-# Create a sidebar for buttons
-with st.sidebar:
-    st.subheader("Select a Feature:")
-    selected_feature = st.selectbox("", [
-        "User Registration & Login",
-        "Profile Management",
-        "Appointment Scheduling",
-        "Medical Records Access",
-        "Symptom Checker",
-    ])
-    if selected_feature == "User Registration & Login":
-        st.subheader("User Registration / Login Area:")
-        username = st.text_input("Username", "")
-        password = st.text_input("Password", "", type="password")
-        registration_button = st.button("Register")
-        login_button = st.button("Login")
-        if registration_button:
-            st.success("Registration Successful!")
-        if login_button:
-            st.success("Login Successful!")
+# Create a selectbox for feature selection
+selected_feature = st.selectbox("Select a Feature:", [
+    "User Registration & Login",
+    "Profile Management",
+    "Appointment Scheduling",
+    "Medical Records Access",
+    "Symptom Checker",
+])
 
 # Display feature details or registration/login area
+if selected_feature == "User Registration & Login":
+    st.subheader("User Registration / Login Area:")
+    username = st.text_input("Username", "")
+    password = st.text_input("Password", "", type="password")
+    registration_button = st.button("Register")
+    login_button = st.button("Login")
+    if registration_button:
+        st.success("Registration Successful!")
+    if login_button:
+        st.success("Login Successful!")
+
+# Add an empty space
 if selected_feature:
     st.write(f"This is where the {selected_feature} details will appear.")
